47dfad475c3980693d9a385e769a3a40ea7ec17e
InventBoss <marlowlichty@gmail.com>
2025-01-22 13:02:51 -0700

    Implemented a settings resource that is change from a file and moved movement systems and child functions to a separate file.

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,38 @@
 use bevy::{
     diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
     prelude::*,
+    window::Window,
 };
+use config::Config;
 use rand::Rng;
+use std::collections::HashMap;
 
-// The starting window dimensions
-pub const WINDOW_WIDTH: f32 = 1000.0;
-pub const WINDOW_HEIGHT: f32 = 600.0;
+mod position_systems;
+use position_systems::{
+    avoid, in_detection_range, is_colliding, move_towards, wiggle_squares, window_collision,
+    PositionSize,
+};
 
-// Initial values to impact the simulation
-pub const PREDATOR_POPULATION: i32 = 10;
-pub const PREY_POPULATION: i32 = 100000;
-
-// Speed that the two populations can go when avoiding and hunting
-pub const PREDATOR_SPEED: f32 = 0.5;
-pub const PREY_SPEED: f32 = 0.5;
-
-// Each population's life force
-pub const PREDATOR_LIFE: i32 = 30000;
-pub const PREY_LIFE: i32 = 50000;
-
-// Range that the populations can "see" each other
-pub const DETECTION_RANGE: f32 = 60.0;
-
-// Size of the entities (e.g. 4 = 4 pixels long by 4 pixels wide)
-pub const DEFAULT_DIMENSION: f32 = 0.5;
-
-// Different environment variables
-pub const ENVIRONMENT_GROW_RATE: f32 = 1.5;
-pub const ENVIRONMENT_MAX: i32 = 1000000;
-
-#[derive(Component)]
-struct PositionSize {
-    x: f32,
-    y: f32,
-    width: f32,
-    height: f32,
+#[derive(Resource)]
+struct Settings {
+    window_width: f32,
+    window_height: f32,
+    predator_population: i32,
+    prey_population: i32,
+    predator_speed: f32,
+    prey_speed: f32,
+    predator_life: i32,
+    prey_life: i32,
+    detection_range: f32,
+    default_dimensions: f32,
+    environment_grow_rate: f32,
+    environment_max: i32,
 }
 
 #[derive(Component)]
 struct Mortal {
     dead: bool,
 }
-
-// #[derive(Component)]
-// struct Direction {
-//     x: f32,
-//     y: f32,
-// }
 
 #[derive(Component)]
 struct Prey;
@@ -63,50 +48,12 @@ struct Life {
 #[derive(Component)]
 struct Environment {
     energy_pool: i32,
-    max_energy: i32,
-}
-
-fn is_colliding(entity1: &PositionSize, entity2: &PositionSize) -> bool {
-    // Used this resource for intersections https://silentmatt.com/rectangle-intersection/
-
-    if entity1.x < entity2.x + entity2.width
-        && entity1.x + entity1.width > entity2.x
-        && entity1.y < entity2.y + entity2.height
-        && entity1.y + entity1.height > entity2.y
-    {
-        return true;
-    }
-    return false;
-}
-
-fn in_detection_range(entity1: &PositionSize, entity2: &PositionSize) -> (bool, f32) {
-    // Formula from https://www.calculator.net/distance-calculator.html
-    let distance = ((entity1.x - entity2.x).powf(2.0) + (entity1.y - entity2.y).powf(2.0)).sqrt();
-
-    return (distance <= DETECTION_RANGE, distance);
-}
-
-fn avoid(entity: &mut PositionSize, target: &PositionSize, speed: f32) {
-    // This sweet answer obtained from
-    // https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space
-    let angle = (target.y - entity.y).atan2(target.x - entity.x);
-
-    entity.x += angle.cos() * -1.0 * speed;
-    entity.y += angle.sin() * -1.0 * speed;
-}
-
-fn move_towards(entity: &mut PositionSize, target: &PositionSize, speed: f32) {
-    // This sweet answer obtained from
-    // https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space
-    let angle = (target.y - entity.y).atan2(target.x - entity.x);
-
-    entity.x += angle.cos() * speed;
-    entity.y += angle.sin() * speed;
 }
 
 fn update_predators(
     mut predators: Query<&mut PositionSize, (With<Predator>, Without<Prey>)>,
     preys: Query<&PositionSize, (With<Prey>, Without<Predator>)>,
+    settings: Res<Settings>,
 ) {
     for mut predator_position_size in predators.iter_mut() {
         // Store the closest position of a prey
@@ -117,8 +64,11 @@ fn update_predators(
         let mut closest_prey_distance: f32 = f32::MAX;
 
         for prey_position_size in preys.iter() {
-            let (detected, distance) =
-                in_detection_range(&predator_position_size, &prey_position_size);
+            let (detected, distance) = in_detection_range(
+                &predator_position_size,
+                &prey_position_size,
+                settings.detection_range,
+            );
 
             if detected && distance < closest_prey_distance {
                 closest_prey_position = Some(prey_position_size);
@@ -129,7 +79,11 @@ fn update_predators(
         // This code checks to see if there is a closest prey position
         // and assigns closest prey the value to pass to the move_towards function
         if let Some(closest_prey) = closest_prey_position {
-            move_towards(&mut predator_position_size, closest_prey, PREDATOR_SPEED);
+            move_towards(
+                &mut predator_position_size,
+                closest_prey,
+                settings.predator_speed,
+            );
         }
     }
 }
@@ -138,6 +92,7 @@ fn update_preys(
     mut preys: Query<(&mut PositionSize, &mut Life), (With<Prey>, Without<Predator>)>,
     predators: Query<&PositionSize, (With<Predator>, Without<Prey>)>,
     mut environment_query: Query<&mut Environment>,
+    settings: Res<Settings>,
 ) {
     for (mut prey_position_size, mut life) in preys.iter_mut() {
         // Store the closest position of a predator
@@ -148,8 +103,11 @@ fn update_preys(
         let mut closest_predator_distance: f32 = f32::MAX;
 
         for predator_position_size in predators.iter() {
-            let (detected, distance) =
-                in_detection_range(&prey_position_size, &predator_position_size);
+            let (detected, distance) = in_detection_range(
+                &prey_position_size,
+                &predator_position_size,
+                settings.detection_range,
+            );
 
             if detected && distance < closest_predator_distance {
                 closest_predator_position = Some(predator_position_size);
@@ -160,10 +118,14 @@ fn update_preys(
         // This code checks to see if there is a closest predator position
         // and assigns closest predator the value to pass to the avoid function
         if let Some(closest_predator) = closest_predator_position {
-            avoid(&mut prey_position_size, closest_predator, PREY_SPEED);
+            avoid(
+                &mut prey_position_size,
+                closest_predator,
+                settings.prey_speed,
+            );
         }
 
-        // Prey "eats" the environment to regain life.
+        // Prey "eats" the environment to regain life
         for mut environment in environment_query.iter_mut() {
             if environment.energy_pool > 0 {
                 environment.energy_pool -= 1;
@@ -173,11 +135,11 @@ fn update_preys(
     }
 }
 
-fn update_environment(mut query: Query<&mut Environment>) {
+fn update_environment(mut query: Query<&mut Environment>, settings: Res<Settings>) {
     for mut environment in query.iter_mut() {
-        environment.energy_pool = ((environment.energy_pool as f32 * ENVIRONMENT_GROW_RATE).round()
-            as i32)
-            .min(environment.max_energy);
+        environment.energy_pool =
+            ((environment.energy_pool as f32 * settings.environment_grow_rate).round() as i32)
+                .min(settings.environment_max);
     }
 }
 
@@ -230,28 +192,6 @@ fn update_transform(mut query: Query<(&PositionSize, &mut Transform)>) {
     }
 }
 
-fn window_collision(mut query: Query<&mut PositionSize>) {
-    for mut position_size in query.iter_mut() {
-        position_size.x = position_size.x.min(WINDOW_WIDTH / 2.0);
-        position_size.x = position_size.x.max(WINDOW_WIDTH / -2.0);
-
-        position_size.y = position_size.y.min(WINDOW_HEIGHT / 2.0);
-        position_size.y = position_size.y.max(WINDOW_HEIGHT / -2.0);
-    }
-}
-
-fn wiggle_squares(_time: Res<Time>, mut query: Query<&mut PositionSize>) {
-    for mut position_size in query.iter_mut() {
-        let random_x: f32 = rand::thread_rng().gen_range(-1.0..1.0);
-        let random_y: f32 = rand::thread_rng().gen_range(-1.0..1.0);
-
-        let wiggle_amount = Vec2::from_array((random_x, random_y).into());
-
-        position_size.x += wiggle_amount.x;
-        position_size.y += wiggle_amount.y;
-    }
-}
-
 fn update_ui_text(
     mut text_query: Query<&mut Text>,
     environment_query: Query<&Environment>,
@@ -271,12 +211,11 @@ fn update_ui_text(
     }
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, settings: Res<Settings>) {
     commands.spawn(Camera2d::default());
 
     commands.spawn(Environment {
-        energy_pool: ENVIRONMENT_MAX / 2,
-        max_energy: ENVIRONMENT_MAX,
+        energy_pool: settings.environment_max / 2,
     });
 
     // Import font and use it to create ui text elements.
@@ -298,29 +237,33 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
     ));
 
+    let window_width: f32 = settings.window_width;
+    let window_height: f32 = settings.window_height;
+
+    let default_dimensions: f32 = settings.default_dimensions;
+
     // Spawn all the initial predators into the simulation
-    for _i in 1..=PREDATOR_POPULATION {
+    for _i in 1..=settings.predator_population {
         let random_x: f32 =
-            rand::thread_rng().gen_range((-(WINDOW_WIDTH / 2.0).abs())..(WINDOW_WIDTH / 2.0).abs());
+            rand::thread_rng().gen_range((-(window_width / 2.0).abs())..(window_width / 2.0).abs());
         let random_y: f32 = rand::thread_rng()
-            .gen_range((-(WINDOW_HEIGHT / 2.0).abs())..(WINDOW_HEIGHT / 2.0).abs());
+            .gen_range((-(window_height / 2.0).abs())..(window_height / 2.0).abs());
 
         commands.spawn((
             Predator,
             Mortal { dead: false },
-            // Direction { x: 0.0, y: 0.0 },
             Life {
-                value: PREDATOR_LIFE,
+                value: settings.predator_life,
             },
             PositionSize {
                 x: random_x,
                 y: random_y,
-                width: DEFAULT_DIMENSION,
-                height: DEFAULT_DIMENSION,
+                width: default_dimensions,
+                height: default_dimensions,
             },
             Sprite {
                 color: Color::srgb(1.0, 0.0, 0.0),
-                custom_size: Some(Vec2::new(DEFAULT_DIMENSION, DEFAULT_DIMENSION)),
+                custom_size: Some(Vec2::new(default_dimensions, default_dimensions)),
                 ..default()
             },
             Transform::from_xyz(random_x, random_y, 0.0),
@@ -328,24 +271,27 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     }
 
     // Spawn all the initial prey into the simulation
-    for _i in 1..=PREY_POPULATION {
-        let random_x: f32 = rand::thread_rng().gen_range(-500.0..500.0);
-        let random_y: f32 = rand::thread_rng().gen_range(-300.0..300.0);
+    for _i in 1..=settings.prey_population {
+        let random_x: f32 =
+            rand::thread_rng().gen_range((-(window_width / 2.0).abs())..(window_width / 2.0).abs());
+        let random_y: f32 = rand::thread_rng()
+            .gen_range((-(window_height / 2.0).abs())..(window_height / 2.0).abs());
 
         commands.spawn((
             Prey,
             Mortal { dead: false },
-            // Direction { x: 0.0, y: 0.0 },
-            Life { value: PREY_LIFE },
+            Life {
+                value: settings.prey_life,
+            },
             PositionSize {
                 x: random_x,
                 y: random_y,
-                width: DEFAULT_DIMENSION,
-                height: DEFAULT_DIMENSION,
+                width: default_dimensions,
+                height: default_dimensions,
             },
             Sprite {
                 color: Color::srgb(0.0, 1.0, 0.0),
-                custom_size: Some(Vec2::new(DEFAULT_DIMENSION, DEFAULT_DIMENSION)),
+                custom_size: Some(Vec2::new(default_dimensions, default_dimensions)),
                 ..default()
             },
             Transform::from_xyz(random_x, random_y, 0.0),
@@ -353,14 +299,55 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     }
 }
 
+fn read_settings(mut commands: Commands) {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("Settings.toml"))
+        .add_source(config::Environment::with_prefix("APP"))
+        .build()
+        .unwrap()
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap();
+
+    // DO NOT MESS UP THE TYPE IN THE CONFIG
+    commands.insert_resource(Settings {
+        window_width: settings["window_width"].parse::<f32>().unwrap(),
+        window_height: settings["window_height"].parse::<f32>().unwrap(),
+        predator_population: settings["predator_population"].parse::<i32>().unwrap(),
+        prey_population: settings["prey_population"].parse::<i32>().unwrap(),
+        predator_speed: settings["predator_speed"].parse::<f32>().unwrap(),
+        prey_speed: settings["prey_speed"].parse::<f32>().unwrap(),
+        predator_life: settings["predator_life"].parse::<i32>().unwrap(),
+        prey_life: settings["prey_life"].parse::<i32>().unwrap(),
+        detection_range: settings["detection_range"].parse::<f32>().unwrap(),
+        default_dimensions: settings["default_dimensions"].parse::<f32>().unwrap(),
+        environment_grow_rate: settings["environment_grow_rate"].parse::<f32>().unwrap(),
+        environment_max: settings["environment_max"].parse::<i32>().unwrap(),
+    });
+
+    println!("{:#?}", settings["window_width"]);
+}
+
 fn main() {
     let mut app = App::new();
+
+    // This is done to get window dimensions on startup only
+    let window_settings = Config::builder()
+        .add_source(config::File::with_name("Settings.toml"))
+        .add_source(config::Environment::with_prefix("APP"))
+        .build()
+        .unwrap()
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap();
 
     app.add_plugins((
         DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Predator Prey Simulation".to_string(),
-                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                resolution: (
+                    window_settings["window_width"].parse::<f32>().unwrap(),
+                    window_settings["window_height"].parse::<f32>().unwrap(),
+                )
+                    .into(),
                 ..default()
             }),
             ..default()
@@ -368,7 +355,7 @@ fn main() {
         FrameTimeDiagnosticsPlugin,
     ));
 
-    app.add_systems(Startup, setup);
+    app.add_systems(Startup, (read_settings, setup.after(read_settings)));
     app.add_systems(
         Update,
         (
---
+++ src/position_systems.rs
@@ -0,0 +1,89 @@
+/*
+    This file includes all systems & child functions used for
+    anything involving moving the important entities (predators & prey) around on the screen.
+    
+    Examples:
+    - Functions that draw the entity squares at said position
+    - Functions that get predators or prey to move based on random behavior, 
+      hunting/defending, etc.
+    - Functions that restrict entity movement to the bounds of the window
+*/
+
+use bevy::prelude::*;
+use rand::Rng;
+
+#[derive(Component)]
+pub struct PositionSize {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+pub fn is_colliding(entity1: &PositionSize, entity2: &PositionSize) -> bool {
+    // Used this resource for intersections https://silentmatt.com/rectangle-intersection/
+
+    if entity1.x < entity2.x + entity2.width
+        && entity1.x + entity1.width > entity2.x
+        && entity1.y < entity2.y + entity2.height
+        && entity1.y + entity1.height > entity2.y
+    {
+        return true;
+    }
+    return false;
+}
+
+pub fn avoid(entity: &mut PositionSize, target: &PositionSize, speed: f32) {
+    // This sweet answer obtained from
+    // https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space
+    let angle = (target.y - entity.y).atan2(target.x - entity.x);
+
+    entity.x += angle.cos() * -1.0 * speed;
+    entity.y += angle.sin() * -1.0 * speed;
+}
+
+pub fn move_towards(entity: &mut PositionSize, target: &PositionSize, speed: f32) {
+    // This sweet answer obtained from
+    // https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space
+    let angle = (target.y - entity.y).atan2(target.x - entity.x);
+
+    entity.x += angle.cos() * speed;
+    entity.y += angle.sin() * speed;
+}
+
+pub fn in_detection_range(
+    entity1: &PositionSize,
+    entity2: &PositionSize,
+    detection_range: f32,
+) -> (bool, f32) {
+    // Formula from https://www.calculator.net/distance-calculator.html
+    let distance = ((entity1.x - entity2.x).powf(2.0) + (entity1.y - entity2.y).powf(2.0)).sqrt();
+
+    return (distance <= detection_range, distance);
+}
+
+pub fn wiggle_squares(_time: Res<Time>, mut query: Query<&mut PositionSize>) {
+    for mut position_size in query.iter_mut() {
+        let random_x: f32 = rand::thread_rng().gen_range(-1.0..1.0);
+        let random_y: f32 = rand::thread_rng().gen_range(-1.0..1.0);
+
+        let wiggle_amount = Vec2::from_array((random_x, random_y).into());
+
+        position_size.x += wiggle_amount.x;
+        position_size.y += wiggle_amount.y;
+    }
+}
+
+pub fn window_collision(mut query: Query<&mut PositionSize>, windows: Query<&Window>) {
+    let window = windows.get_single().unwrap();
+    let window_width: f32 = window.width();
+    let window_height: f32 = window.height();
+
+    for mut position_size in query.iter_mut() {
+        position_size.x = position_size.x.min(window_width / 2.0);
+        position_size.x = position_size.x.max(window_width / -2.0);
+
+        position_size.y = position_size.y.min(window_height / 2.0);
+        position_size.y = position_size.y.max(window_height / -2.0);
+    }
+}
